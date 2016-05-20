ff3c8a9663377518269dd99109ea789c5223bc59
jeffhsu-sickkids <jeff.hsu@sickkids.ca>
2016-05-20 13:34:17 -0400

    added some test templates

--- publish/publish_test.go
+++ publish/publish_test.go
@@ -2,12 +2,4 @@ package main
 
 import (
     "testing"
-
 )
-
-func TestTruth(t *testing.T) {
-    getRaw("data.json")
-    if true != true {
-        t.Error("everything I know is wrong")
-    }
-}
---
+++ subscribe/subscribe_test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+    "testing"
+    "reflect"
+)
+
+func TestConvertMap(t *testing.T) {
+    b := []byte(`{"Name":"Wednesday","Age":6,"Parents":["Gomez","Morticia"]}`)
+    f := map[string]interface{}{
+        "Name": "Wednesday",
+        "Age":  6,
+        "Parents": []interface{}{
+            "Gomez",
+            "Morticia",
+        },
+    }
+    if result := convertToMap(b); reflect.DeepEqual(result, m){
+        t.Error("expected", f, "got", result)
+    }
+}
