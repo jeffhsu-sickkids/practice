035435470c9dacb317370552edbbf155d87cc8dc
Edgar Crowdy <crowdy@sickkids.ca>
2016-05-24 12:10:14 -0400

    an example test and some changes to publish

--- publish/publish.go
+++ publish/publish.go
@@ -1,54 +1,66 @@
 package main
 
 import (
+	"errors"
 	"flag"
-	"log"
 	"github.com/nats-io/nats"
-    "io/ioutil"
+	"io/ioutil"
+	"log"
 )
 
-func usage(){
-    log.Fatalf("Usage: [-s server (%s)] [-sub subject] <file>\n", nats.DefaultURL)
+func usage() {
+	log.Fatalf("Usage: [-s server (%s)] [-sub subject] <file>\n", nats.DefaultURL)
 }
 
 // This function reads from a text file and returns the raw data of JSON
-func getRaw(fpath string) []byte {
-    raw, err := ioutil.ReadFile(fpath)
-    if err != nil {
-        panic(err)
-    }
-    return raw
+func getRaw(fpath string) ([]byte, error) {
+	raw, err := ioutil.ReadFile(fpath)
+	if err != nil {
+		return nil, err
+	}
+	return raw, nil
 }
 
-func main(){
+func main() {
 
-    // Defining command-line flags and default values
-    var urls = flag.String("s", nats.DefaultURL, "nats server URLs")
-    var sub = flag.String("sub", "CVDMC", "subject to publish/subscribe on")
+	// Defining command-line flags and default values
+	var urls = flag.String("s", nats.DefaultURL, "nats server URLs")
+	var sub = flag.String("sub", "CVDMC", "subject to publish/subscribe on")
 
-    log.SetFlags(0)
-    flag.Usage = usage
-    flag.Parse()
+	log.SetFlags(0)
+	flag.Usage = usage
+	flag.Parse()
 
 	args := flag.Args()
 	if len(args) < 1 {
 		usage()
 	}
 
-    // Connects to nats server
-    nc, err := nats.Connect(*urls)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer nc.Close()
+	var fileName string = args[0]
 
-    msg := getRaw(args[0])
+	_ = pub(urls, sub, fileName)
+}
 
-    // Publish the data
-    nc.Publish(*sub, msg)
+func pub(urls *string, sub *string, fileName string) error {
+
+	// Connects to nats server
+	nc, err := nats.Connect(*urls)
+	if err != nil {
+		log.Fatal(err)
+		return errors.New("a problem with connecting to NATS")
+	}
+	defer nc.Close()
+
+	msg, err := getRaw(fileName)
+	if err != nil {
+		return err
+	}
+
+	// Publish the data
+	nc.Publish(*sub, msg)
 	nc.Flush()
 
-    log.Printf("Published [%s] : '%s'\n", *sub, msg)
+	log.Printf("Published [%s] : '%s'\n", *sub, msg)
 
-
+	return nil
 }
--- publish/publish_test.go
+++ publish/publish_test.go
@@ -1,5 +1,18 @@
 package main
 
 import (
-    "testing"
+	"github.com/nats-io/nats"
+	"testing"
 )
+
+func TestMissingFile(t *testing.T) {
+	var urls string = nats.DefaultURL
+	var sub string = "test missing file"
+	var fileName string = "bar" // there is no input file with this name
+
+	err := pub(&urls, &sub, fileName)
+	if err != nil {
+		t.Error(err)
+	}
+
+}
